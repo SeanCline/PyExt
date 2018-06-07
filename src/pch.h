234c52e19b01018d5e010d506ab1c2b850f7174b
Sean D. Cline <sean.d.cline@gmail.com>
2018-06-07 07:41:48 -0400

    Disable warning C5040 for engextcpp.hpp and engextcpp.cpp. They still use the legacy C++03 throws() clause and there's not much I can do about it without forking them.

--- src/pch.h
+++ src/pch.h
@@ -3,6 +3,7 @@
 // Disable warnings from the Debugging Tools for Windows headers.
 #pragma warning( push )
 #pragma warning( disable : 4838 )
+#pragma warning( disable : 5040 )
 #include <engextcpp.hpp>
 #pragma warning( pop ) 
 
