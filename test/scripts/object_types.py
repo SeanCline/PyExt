66a8f31cc90c9f3f932eb81ad900e7b3fc33e032
Sean D. Cline <sean@sdcline.com>
2026-05-30 20:33:10 -0400

    Fix unit tests.
    `object_types.py` had some Python 3-only synta sneak in. Since the same
    test harness is used by Python 2 and 3, the test scripts need to at
    least run on Python 2.

--- test/scripts/object_types.py
+++ test/scripts/object_types.py
@@ -34,38 +34,49 @@ dict_obj = {
 	"long_obj": long_obj,
 }
 
-ellipsis_obj = ...
+ellipsis_obj = Ellipsis # Need to use the type because Python 2 doesn't support the `...` literal.
 
 slice_obj = slice(1, 10, 2)
 
-range_obj = range(1, 10, 2)
+range_obj = range(1, 10, 2) # Ranges in Python 2 are just list's but that's Ok for our purposes.
 
 memoryview_obj = memoryview(bytes_obj)
 
 import weakref
-class _Referent: pass
-_keep_alive = _Referent()
-weakref_obj = weakref.ref(_keep_alive)
+class Referent(object): pass
+strong_ref = Referent()
+weakref_obj = weakref.ref(strong_ref)
 
-class _Base:
-	def foo(self): return "base"
-class _Derived(_Base):
-	def foo(self): return super()
-super_obj = _Derived().foo()
+class Derived(object):
+    def my_super(self): return super(Derived, self) # Use the verbose `super()` to support Python 2
+super_obj = Derived().my_super()
 
-def _gen():
+# Generators (`yield`) and `next()` work on Py2.6+.
+def gen():
 	yield 1
 	yield 2
-generator_obj = _gen()
-next(generator_obj) # suspend at the first yield so gi_iframe is populated.
+generator_obj = gen()
+next(generator_obj) # Suspend at the first yield so the iframe member is populated.
 
-async def _coro():
-	return 42
+# `async def` was added in Python 3.5, so we have to squash any parse errors in older versions.
+try:
+	exec(
+		"async def coro():\n"
+		"    return 42\n"
+		"coroutine_obj = coro()\n"
+	)
+except SyntaxError:
+	pass
 
-coroutine_obj = _coro()
+# `async def`+`yield` was added in Python 3.6, so we have to squash any parse errors in older versions.
+try:
+	exec(
+		"async def _agen():\n"
+		"    yield 1\n"
+		"async_generator_obj = _agen()\n"
+	)
+except SyntaxError:
+	pass
 
-async def _agen():
-	yield 1
-async_generator_obj = _agen()
-
+# Generate the dump with all of the objects above in the global scope for the test harness to inspect.
 win32debug.dump_process("object_types.dmp")
